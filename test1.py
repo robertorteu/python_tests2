d9ba9eb5fbde7c5bfd3919c95371e1c365ac3e68
Robert Orteu <robert.orteu@propellingtech.com>
2024-03-26 10:05:34 +0100

    first push to Git

---
+++ test1.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+
+print("Hello World")
+print("How are you doing?")
+print("Mangueta a full")
+
+# Read the Excel file
+data = pd.read_excel(r'C:\Users\RobertOrteu\OneDrive - Propelling Tech S.L\Documents\python_tests\dataset1.xlsx')
+
+# Filter the data by Location = "Lleida"
+filtered_data = data[data['Location'] == 'Lleida']
+
+# Calculate the sum of the Amounts column
+total_amount = filtered_data['Amount'].sum()
+
+# Print the result
+print(total_amount)
+# Generate a new Excel file with the total_amount value
+filtered_data['Total_Amount'] = total_amount
+output_file = 'C:\\Users\\RobertOrteu\\OneDrive - Propelling Tech S.L\\Documents\\python_tests\\dataset1_outcome.xlsx'
+filtered_data.to_excel(output_file, index=False)
